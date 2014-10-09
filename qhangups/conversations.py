e88b411fc75093469c5d19d4c8d264185658ece2
Michal Krenek (Mikos) <m.krenek@gmail.com>
2014-10-09 10:24:24 +0200

    Close tabs by middle mouse click

--- qhangups/conversations.py
+++ qhangups/conversations.py
@@ -18,6 +18,19 @@ class QHangupsConversations(QtGui.QDialog, Ui_QHangupsConversations):
         self.conversationsTabWidget.currentChanged.connect(self.on_tab_current_changed)
         self.conversationsTabWidget.tabCloseRequested.connect(self.on_tab_close_requested)
 
+        # Install ourselves as event filter so we can catch middle mouse button (see eventFilter method)
+        self.conversationsTabWidget.tabBar().installEventFilter(self)
+
+    def eventFilter(self, obj, event):
+        """Event filter for catching middle mouse button on tab and closing it"""
+        if obj is self.conversationsTabWidget.tabBar():
+            if event.type() == QtCore.QEvent.MouseButtonRelease and event.button() == QtCore.Qt.MidButton:
+                tab_index = obj.tabAt(event.pos())
+                if tab_index != -1:
+                    obj.tabCloseRequested.emit(tab_index)
+                    return True
+        return super().eventFilter(obj, event)
+
     def get_conv_widget(self, conv_id):
         """Return an existing or new QHangupsConversationWidget"""
         if conv_id not in self.conv_widgets:
