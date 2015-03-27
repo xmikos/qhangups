d9784e886c614f347d8c4bfffbd27a40df05bfe1
Michal Krenek (Mikos) <m.krenek@gmail.com>
2015-03-27 21:40:07 +0100

    Update flake8_test.sh - add _ to builtins (for gettext compatibility), don't test if import is at top of file, raise max complexity to 8

--- flake8_test.sh
+++ flake8_test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-flake8 --exclude="qrc_*,ui_*" --ignore=E401 --max-line-length 119 --max-complexity 6 --show-source "$@"
+flake8 --exclude="qrc_*,ui_*" --builtins="_" --ignore=E401,E402 --max-line-length 119 --max-complexity 8 --show-source "$@"
