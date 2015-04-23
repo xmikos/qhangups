23d5e45c1f9bfb2e29f630cbc5106be8d2b60467
Michal Krenek (Mikos) <m.krenek@gmail.com>
2015-04-23 17:21:37 +0200

    Use new ReParser-based message parser from Hangups (supports simple Markdown and HTML syntax)

--- qhangups/utils.py
+++ qhangups/utils.py
@@ -7,22 +7,7 @@ logger = logging.getLogger(__name__)
 
 def text_to_segments(text):
     """Create list of ChatMessageSegments from text"""
-    # Replace two consecutive spaces with space and non-breakable space,
-    # then split text to lines
-    lines = text.replace('  ', ' \xa0').splitlines()
-    if not lines:
-        return []
-
-    # Generate line segments
-    segments = []
-    for line in lines[:-1]:
-        if line:
-            segments.append(hangups.ChatMessageSegment(line))
-        segments.append(hangups.ChatMessageSegment('\n', hangups.SegmentType.LINE_BREAK))
-    if lines[-1]:
-        segments.append(hangups.ChatMessageSegment(lines[-1]))
-
-    return segments
+    return list(hangups.ChatMessageSegment.from_str(text))
 
 
 def segment_to_html(segment):
